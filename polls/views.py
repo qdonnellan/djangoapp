265a151affec3e3bda8edcdc99b6d33ed69f1376
qdonnellan <qdonnellan@gmail.com>
2013-12-28 16:35:06 -0600

    initial commit

---
+++ polls/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from polls.models import Poll
+
+def index(request):
+  latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+  output = ', '.join([p.question for p in latest_poll_list])
+  return HttpResponse(output)
+
+def detail(request, poll_id):
+  return HttpResponse("You are looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+  return HttpResponse("You are looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+  return HttpResponse("You are voting on poll %s." % poll_id)
